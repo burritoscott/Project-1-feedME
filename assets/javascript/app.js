fe95cc7e3816513d8c3fa21af8e546424cfc3aca
lquijano <quijano1012@gmail.com>
2018-07-18 00:40:22 -0700

    added artist photos, input buttons with placeholders, ajax call to bandsintown api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,32 @@
-$(document).ready(function() {
+  $('#artistSearch').on('click', function(){
+    event.preventDefault();
 
-<script src="https://www.gstatic.com/firebasejs/5.2.0/firebase.js"></script>
-  <script>
-    // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyDvedVAaDbkNiaSTFnzGeclFNIbqBIjyqU",
-      authDomain: "feedmeproject-e83a3.firebaseapp.com",
-      databaseURL: "https://feedmeproject-e83a3.firebaseio.com",
-      projectId: "feedmeproject-e83a3",
-      storageBucket: "feedmeproject-e83a3.appspot.com",
-      messagingSenderId: "557116830448"
-    };
-    firebase.initializeApp(config);
+    var artist = $('#inputBand').val();
 
-    var database = firebase.database();
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist +
+    "app_id=codingbootcamp";
 
--------------------------------------------------------------
-    $('input').on('submit', function(){
-      event.preventDefault();
-
-      var restaurantName = ;
-      var url = 'https://api.yelp.com/v3' + ' ';
-
-
-/businesses/search
-/businesses/{id}/reviews
+$.ajax({
+  url: queryURL,
+}).then(function(response) {
+    console.log(response);
+    // var results = response.data;
     });
+  });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </script>
+// <script src="https://www.gstatic.com/firebasejs/5.2.0/firebase.js"></script>
+//   <script>
+//     // Initialize Firebase
+//     var config = {
+//       apiKey: "AIzaSyDvedVAaDbkNiaSTFnzGeclFNIbqBIjyqU",
+//       authDomain: "feedmeproject-e83a3.firebaseapp.com",
+//       databaseURL: "https://feedmeproject-e83a3.firebaseio.com",
+//       projectId: "feedmeproject-e83a3",
+//       storageBucket: "feedmeproject-e83a3.appspot.com",
+//       messagingSenderId: "557116830448"
+//     };
+//     firebase.initializeApp(config);
+//
+//     var database = firebase.database();
